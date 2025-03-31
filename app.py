c14b0fd61904c00d715ebd097e39a98ba6603292
arunabai0452 <arunabai0452@gmail.com>
2025-03-31 03:40:18 -0500

    Feat: initial commit

--- app.py
+++ app.py
@@ -41,4 +41,5 @@ def handle_disconnect():
 
 # Run the app
 if __name__ == '__main__':
-    socketio.run(app, host='0.0.0.0', port=5001)
+    port = int(os.environ.get('PORT', 5000)) 
+    socketio.run(app, host='0.0.0.0', port=port)
